4c7a09eaceaf82af737cfadbe53929ed26a2b19e
Robert Greenawalt <robert.greenawalt29@gmail.com>
2020-10-22 20:16:22 -0400

    created virtual for total duration of workouts

--- models/workout.js
+++ models/workout.js
@@ -37,9 +37,13 @@ const WorkoutSchema = new Schema({
         }
     ]
 }, {
-    toJSON: { virtuals: true }
-}
+    toJSON: { virtuals: true }}
 );
+WorkoutSchema.virtual('totalDuration').get(function() {
+    return this.exercises.reduce((accumulator, increment)=>{
+        return accumulator + increment.duration
+    },0);
+  });
 
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
